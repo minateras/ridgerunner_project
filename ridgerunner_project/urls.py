49e81205af712efaae4b7b93d46f0fec0aeeb3ca
Mina Teräs <alvaminaelvirateras@icloud.com>
2024-05-15 18:43:48 +0200

    defined root

--- ridgerunner_project/urls.py
+++ ridgerunner_project/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+ridgerunner_app_urls = include('ridgerunner_app.urls')
+
 urlpatterns = [
-    path('ridgerunner_app/', include('ridgerunner_app.urls')),
+    path('', ridgerunner_app_urls),
+    path('ridgerunner_app/', ridgerunner_app_urls),
     # path('workingridge_app/', include('workingridge_app.urls')),
     path('admin/', admin.site.urls),
 ]
